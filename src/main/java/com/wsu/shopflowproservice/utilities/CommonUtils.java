1d388de7f48411d42de4c1f609ec5b4159b78667
Toast <ashupatel2121@gmail.com>
2024-10-24 00:25:53 -0400

    assignment 7 setup

--- src/main/java/com/wsu/shopflowproservice/utilities/CommonUtils.java
+++ src/main/java/com/wsu/shopflowproservice/utilities/CommonUtils.java
@@ -1,4 +1,4 @@
-package com.wsu.workorderproservice.utilities;
+package com.wsu.shopflowproservice.utilities;
 
 import lombok.experimental.UtilityClass;
 import org.springframework.data.domain.Sort;
